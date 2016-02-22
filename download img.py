71b24422f8b9016260d9a61dfde5c0ac0ce7c9b4
I0T <1121887411@qq.com>
2016-02-23 02:56:33 +0800

    Update download img.py

--- download img.py
+++ download img.py
@@ -3,25 +3,37 @@
 __author__ = 'IOT'
 import time
 import requests
-time_start = time.time()
-print ('开始下载图片')
-name = 'name'
-f= open ('./%s/%s图片链接.txt'%(name,name),'r')
-f= f.readlines()
-num = len(f)
-false = open('./%s/无法下载的链接.txt'%name,'a+')
-pic_name = 0
-for image in f:
-    image = image.strip('\n')
-    format=image.split(".")[-1]
-    pic_name = pic_name+1
-    try:
-        image = requests.get(image)
-        with open('./%s/%s.%s'%(name,pic_name,format),'wb') as images:
-            images.write(image.content)
-            an =str(100*pic_name/num).split('.')[0]
-            print ('第%s张下载完毕   '%pic_name+an+'%')
-    except:
-            false.writelines(str(image)+'\n')
-    time_now = str(time.time()-time_start).split('.')[0]
-    print ('程序运行时间%ss'%time_now)
+import threading
+class download(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+    def run(self):
+        main()
+def main():
+    time_start = time.time()
+    print ('开始下载图片')
+    name = 'name'
+    f= open ('./%s/%s图片链接.txt'%(name,name),'r')
+    f= f.readlines()
+    num = len(f)
+    false = open('./%s/无法下载的链接.txt'%name,'a+')
+    pic_name = 0
+    for image in f:
+        image = image.strip('\n')
+        format=image.split(".")[-1]
+        pic_name = pic_name+1
+        try:
+            image = requests.get(image)
+            with open('./%s/%s.%s'%(name,pic_name,format),'wb') as images:
+               images.write(image.content)
+               an =str(100*pic_name/num).split('.')[0]
+               print (time.strftime("%X")+'  第%s张下载完毕   '%pic_name+an+'%')
+        except:
+               false.writelines(str(image)+'\n')
+        time_now = str(time.time()-time_start).split('.')[0]
+        print ('程序运行时间%ss'%time_now)
+    for i in range(100):
+           c=download()
+           c.start()
+if __name__=='__main__':
+    main()
